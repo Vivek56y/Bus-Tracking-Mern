b7f8db31888a3e67f0629cc58936685ded0b85f6
vivek <vahi7315@gmail.com>
2026-02-22 19:03:28 +0530

    smtp apply

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Contact from "./components/Contact";
 import BusList from "./components/BusList";
 import Addbuses from "./pages/Addbuses";
 import AddBusForm from "./components/AddBusForm";
-
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
 function App() {
   return (
     <Router>
@@ -20,7 +21,9 @@ function App() {
           <Route path="/BusMapPreview" element={<BusMapPreview />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/AddBusForm" element={<AddBusForm />} />
-          <Route path="/BusList" element={<BusList />} />
+          <Route path="/BusList" element={<BusList />}/>
+          <Route path="/Login" element={<LoginPage/>}/>
+           <Route path="/signup" element={<SignupPage />} /> 
         </Routes>
       </div>
     </Router>
