2b11a3d917058f492f78998ce0f974fc59b76a88
vivek <vahi7315@gmail.com>
2026-02-28 23:40:40 +0530

    user login setup

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "customer"], default: "customer" },
+  role: { type: String, enum: ["user", "customer"], default: "customer" },
   emailVerified: { type: Boolean, default: false },
   otpHash: { type: String, default: null },
   otpExpiresAt: { type: Date, default: null },
